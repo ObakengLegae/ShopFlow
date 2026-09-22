f3142b05458bfc21144c06a2af7fae0166e1a2a8
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-22 22:25:50 +0200

    refactor: main.py to pipeline.py, turning main into a class

---
+++ src/shopflow/pipeline.py
@@ -0,0 +1,31 @@
+from extractor import Extractor
+from transformer import Transformer
+from loader import Loader
+from database.database import Database
+
+class Pipeline:
+
+    def __init__(self):
+        self.sql_file_path = './sql/schema.sql'
+        self.raw_data_path = './data/raw/uci/Online Retail.csv'
+        self.processed_data_path = './data/processed/uci/processed_data.csv'
+        self.database = Database()
+        self.extractor = Extractor()
+        self.transformer = Transformer()
+        self.loader = Loader(database=self.database)
+
+    def run(self):
+        self.database.create_tables(self.sql_file_path)
+
+        raw_data = self.extractor.extract_data(self.raw_data_path)
+
+        transformed_data = self.transformer.transform(raw_data)
+
+        self.transformer.save_transformed_data(transformed_data)
+
+        self.loader.load(self.processed_data_path)
+
+
+if __name__ == '__main__':
+    pipeline = Pipeline()
+    pipeline.run()
