9fea72a49768588b68a61430fee0fc67077eec10
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-22 16:53:01 +0200

    add: load method(will call other insert methods) to load.py

--- src/shopflow/load.py
+++ src/shopflow/load.py
@@ -3,6 +3,25 @@ from numpy.core import records
 from psycopg2.extras import execute_values
 from sqlalchemy.engine import row
 
+from src.shopflow.extract import extract_data
+
+
+def load(file_path: str, database_connection):
+    processed_data = extract_data(file_path)
+    database_connection.connect()
+
+    try:
+        load_customers(processed_data, database_connection)
+
+        database_connection.commit()
+
+    except Exception:
+        database_connection.rollback()
+        raise
+
+    finally:
+        database_connection.close()
+
 
 def load_customers(data: pd.DataFrame, connection):
     customers = data[
