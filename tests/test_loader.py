4e9f3c16dc3750761313948da62fe6760bc26431
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-22 21:23:36 +0200

    added: test for load_customers

---
+++ tests/test_loader.py
@@ -0,0 +1,47 @@
+import pytest
+import pandas as pd
+
+from src.shopflow.database.database import Database
+from src.shopflow.extractor import Extractor
+from src.shopflow.loader import Loader
+
+@pytest.fixture
+def database_instance():
+    database = Database()
+    database.create_tables(database.default_sql)
+    try:
+        database.delete_data("DELETE FROM sales; DELETE FROM customers; DELETE FROM products; DELETE FROM dates")
+    except Exception:
+        pass
+
+    yield database
+
+    try:
+        database.delete_data("DELETE FROM sales; DELETE FROM customers; DELETE FROM products; DELETE FROM dates")
+
+    except Exception:
+        pass
+
+@pytest.fixture
+def loader_instance(database_instance):
+    return Loader(database_instance)
+
+@pytest.fixture
+def sample_data():
+    file_path = "./tests/resources/sample_data.csv"
+    extractor = Extractor()
+    data = extractor.extract_data(file_path)
+    return data
+
+def test_load_customers(database_instance, loader_instance, sample_data):
+    loader_instance.load_customers(sample_data)
+
+    df_result = database_instance.fetch_data("SELECT * FROM customers WHERE customer_id IN ('17850', '13047', '12583');")
+
+    assert not df_result.empty
+    assert len(df_result) == 3
+    assert set(df_result["customer_id"]) == {"17850", "13047", "12583"}
+    assert set(df_result["country"]) == {"United Kingdom", "France"}
+
+
+
