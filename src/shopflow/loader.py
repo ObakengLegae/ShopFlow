5d46d19d3d57243b0b173759bc1879674e6d73e4
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-23 18:03:11 +0200

    refactor: loader.py
    Added more descriptive docstrings
    added exception handling logic to the load() method

--- src/shopflow/loader.py
+++ src/shopflow/loader.py
@@ -1,53 +1,73 @@
+import logging
+
 import pandas as pd
 from src.shopflow.database.database import Database
 from src.shopflow.extractor import Extractor
 
+logger = logging.getLogger(__name__)
+
 class Loader:
+    """Isolates relation data tables and loads them into database"""
     def __init__(self, database: Database):
         self.database = database
 
     def load(self, processed_data: pd.DataFrame):
-        """Adds all data to database"""
-        print("Adding data to database")
-        self.load_customers(processed_data)
-        self.load_products(processed_data)
-        self.load_dates(processed_data)
-        self.load_sales(processed_data)
+        """Loads all the data into the database in the correct relational order."""
+        if processed_data is None or processed_data.empty:
+            logger.warning("Empty DataFrame. Skipping load operations")
+            return
 
+        logger.info("Loading data")
+        try:
+            self.load_customers(processed_data)
+            self.load_products(processed_data)
+            self.load_dates(processed_data)
+            self.load_sales(processed_data)
+            logger.info("All tables successfully loaded")
+        except KeyError as e:
+            logger.error(f"Failed to find column during loading: {e}")
+            raise
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            raise
 
     def load_customers(self, data: pd.DataFrame):
-        """Adds customer data to database"""
+        """Isolates customer data and adds it to database"""
+        logger.info("Isolating customer data")
         customers = data[
             ["customer_id", "country"]
         ].drop_duplicates(subset="customer_id")
 
         self.database.insert_data(customers, "customers")
-        print("Customers added to database")
+        logger.info("Customers sent to database")
 
     def load_products(self, data: pd.DataFrame):
-        """Adds product data to database"""
+        """Isolates product data and adds it to database"""
+        logger.info("Isolating product data")
         products = data[
             ["stock_code", "description"]
         ].drop_duplicates(subset="stock_code")
 
         self.database.insert_data(products, "products")
-        print("Products added to database")
+        logger.info("Products sent to database")
 
     def load_dates(self, data: pd.DataFrame):
-        """Adds dates to database"""
+        """Isolates date data and adds it to database"""
+        logger.info("Isolating date data")
         dates = data[
             ["date_id", "full_date", "year", "month", "day", "quarter", "day_of_week"]
         ].drop_duplicates(subset="date_id")
 
         self.database.insert_data(dates, "dates")
-        print("Dates added to database")
+        logger.info("Dates sent to database")
 
     def load_sales(self, data: pd.DataFrame):
-        """Adds sales data to database"""
+        """Isolates sales data and adds it to database"""
+        logger.info("Isolating sale data")
         sales = data[
             ["invoice_no", "customer_id", "stock_code", "date_id", "quantity", "unit_price", "total_amount",
              "sale_timestamp"]
         ].drop_duplicates(subset="invoice_no")
 
         self.database.insert_data(sales, "sales")
-        print("Sales added to database")
+        logger.info("Sales sent to database")
