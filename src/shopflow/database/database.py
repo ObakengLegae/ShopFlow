f7c6ccc6533e45dd86f56c5c2658e5ec85e0065f
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-23 16:43:47 +0200

    refactor: database.py
    replacing print statements with a logger
    adding exception more exception handling

--- src/shopflow/database/database.py
+++ src/shopflow/database/database.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 import psycopg2
 import pandas as pd
@@ -6,9 +7,17 @@ from psycopg2.extras import execute_values
 from dotenv import load_dotenv
 
 load_dotenv()
-class Database:
+logger = logging.getLogger(__name__)
 
+class Database:
+    """Manages PostgreSQL database connection"""
     def __init__(self):
+        required_variables = ["DB_HOST", "DB_PORT", "DB_USER", "DB_PASSWORD", "DB_NAME"]
+        missing_variables = [var for var in required_variables if not os.getenv(var)]
+        if missing_variables:
+            logger.error(f"Missing required database environment variables: {';'.join(missing_variables)}")
+            raise EnvironmentError(f"Missing required database environment variables: {';'.join(missing_variables)}")
+
         self.host = os.getenv("DB_HOST")
         self.port = os.getenv("DB_PORT")
         self.database = os.getenv("DB_NAME")
@@ -18,34 +27,41 @@ class Database:
         self.default_sql = "./sql/schema.sql"
 
     def connect(self):
-        return psycopg2.connect(
+        """Connects to PostgreSQL database and returns connection."""
+        try:
+            return psycopg2.connect(
             host = self.host,
             port = self.port,
             database = self.database,
             user = self.user,
             password = self.password
         )
+        except psycopg2.OperationalError as e:
+            logger.error(f"Failed to connect to PostgreSQL database: {e}")
+            raise
 
-    def create_tables(self, sql_file):
+    def create_tables(self, sql_file: str):
+        """Executes an SQL file to create database tables."""
+        if not os.path.exists(sql_file):
+            logger.error(f"SQL file not found at path: {sql_file}")
+            raise FileNotFoundError(f"Schema file missing: {sql_file}")
+
+        logger.info(f"Creating tables...")
         connection = self.connect()
-        print("Creating tables...")
-        cursor = connection.cursor()
 
         try:
-            with open(sql_file, "r") as file:
-                sql = file.read()
-
-            cursor.execute(sql)
-            connection.commit()
-        except Exception:
-            connection.rollback()
+            with connection:
+                with connection.cursor() as cursor:
+                    with open(sql_file, "r") as file:
+                        sql = file.read()
+                    cursor.execute(sql)
+            logger.info("Tables created successfully")
+        except Exception as e:
+            logger.error(f"Failed to create tables: {e}")
             raise
         finally:
-            cursor.close()
             connection.close()
-        print("Tables created")
 
-#Todo: Add some validation logic against empty frames and table names
     def insert_data(self, df: pd.DataFrame, table_name: str):
         connection = self.connect()
         print("Inserting data...")
