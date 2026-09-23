79da795783bffebc05d8ce42d5b2b90db955f6e6
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-23 17:12:09 +0200

    add: test_pipeline.py
    Added tests for the new pipeline

---
+++ tests/test_pipeline.py
@@ -0,0 +1,44 @@
+import pytest
+
+from src.shopflow.pipeline import Pipeline
+from src.shopflow.database.database import Database
+
+sample_data_path = "./tests/resources/raw_csv_with_null_values.csv"
+
+@pytest.fixture
+def database_instance():
+    database = Database()
+    database.create_tables(database.default_sql)
+
+    try:
+        database.delete_data("DELETE FROM sales; DELETE FROM customers; DELETE FROM products; DELETE FROM dates")
+    except Exception:
+        pass
+
+    yield database
+
+    try:
+        database.delete_data("DELETE FROM sales; DELETE FROM customers; DELETE FROM products; DELETE FROM dates")
+    except Exception:
+        pass
+
+def test_pipeline_success(database_instance):
+
+    pipeline = Pipeline(
+        sql_file_path=database_instance.sql_file_path,
+        raw_data_path=sample_data_path,
+        database=database_instance
+    )
+    pipeline.run()
+
+    df_customers = database_instance.fetch_data("SELECT * FROM customers;")
+    assert len(df_customers) == 3
+
+    df_products = database_instance.fetch_data("SELECT * FROM products;")
+    assert len(df_products) == 8
+
+    df_dates = database_instance.fetch_data("SELECT * FROM dates;")
+    assert len(df_dates) == 2
+
+    df_sales = database_instance.fetch_data("SELECT DISTINCT invoice_no FROM sales;")
+    assert len(df_sales) == 3
