019600d13c22d4f6461d37d706f7b76f3c4970bc
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-23 09:57:38 +0200

    refactor: pipeline.py
    Added params to the Pipeline class to make it more testable

--- src/shopflow/pipeline.py
+++ src/shopflow/pipeline.py
@@ -3,16 +3,34 @@ from transformer import Transformer
 from loader import Loader
 from database.database import Database
 
-class Pipeline:
-
-    def __init__(self):
-        self.sql_file_path = './sql/schema.sql'
+"""
+self.sql_file_path = './sql/schema.sql'
         self.raw_data_path = './data/raw/uci/Online Retail.csv'
         self.processed_data_path = './data/processed/uci/processed_data.csv'
         self.database = Database()
         self.extractor = Extractor()
         self.transformer = Transformer()
         self.loader = Loader(database=self.database)
+"""
+class Pipeline:
+
+    def __init__(
+            self,
+            sql_file_path: str = None,
+            raw_data_path: str = None,
+            processed_data_path: str = None,
+            database: Database = None,
+            extractor: Extractor = None,
+            transformer: Transformer = None,
+            loader: Loader = None
+    ):
+        self.sql_file_path = sql_file_path or './sql/schema.sql'
+        self.raw_data_path = raw_data_path or './data/raw/online_retail.csv'
+        self.processed_data_path = processed_data_path or './data/processed/online_retail.csv'
+        self.database = database or Database()
+        self.extractor = extractor or Extractor()
+        self.transformer = transformer or Transformer()
+        self.loader = loader or Loader(database=self.database)
 
     def run(self):
         self.database.create_tables(self.sql_file_path)
