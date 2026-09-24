d57a457f3170bfca8a2e0b65e9d742ef5ac8faec
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-24 20:43:48 +0200

    feat: added s3 permission for lambda function

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -21,3 +21,21 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
     role       = aws_iam_role.lambda_exec_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+data "aws_iam_policy_document" "lambda_s3_read" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.data_storage.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_s3_read_policy" {
+  name        = "shopflow-lambda-s3-read-policy"
+  description = "Allows Lambda to read objects from the ShopFlow data landing zone"
+  policy      = data.aws_iam_policy_document.lambda_s3_read.json
+}
