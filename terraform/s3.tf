08b7c3e78e7abe734269f4e97770bed6485d6d9a
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-24 18:09:19 +0200

    feat: s3 bucket implementation
    added terraform configuration for the s3 bucket

---
+++ terraform/s3.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "data_storage" {
+    bucket_prefix = "shopflow-data"
+
+    tags = {
+        Project     = "Shopflow"
+        Environment =
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "data_storage_access" {
+    bucket = aws_s3_bucket.data_storage.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
+
+output "s3_bucket_name" {
+    description = "The name of the s3 bucket."
+    value       = aws_s3_bucket.data_storage.bucket
+}
