e5d125b29da995e92a1b4c0edd1e7ecbfa3f95dc
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-24 22:05:15 +0200

    refactor: converted uploader.py to class
    Uploads files to s3 bucket
    can upload to both the raw/ and processed/ directories

--- src/shopflow/upload.py
+++ src/shopflow/upload.py
@@ -8,31 +8,56 @@ from botocore.exceptions import NoCredentialsError, ClientError
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-def upload_file_to_s3(file_path: str, bucket_name: str, object_name: str = None):
-    """Uploads a local file to an AWS S3 bucket."""
-    if not os.path.exists(file_path):
-        logger.error(f"File not found: {file_path}")
-        return False
+class Uploader:
+    def __init__(self, bucket_name: str = None, s3_prefix: str = "processed/"):
+        """
+        Initializes the Uploader.
+        :param bucket_name: The destination S3 bucket name. If None, uploading is skipped.
+        :param s3_prefix: The folder to upload the file to in s3
+        """
+        self.bucket_name = bucket_name
+        self.s3_prefix = s3_prefix
 
-    if object_name is None:
-        object_name = os.path.basename(file_path)
+        try:
+            self.s3_client = boto3.client('s3')
+        except Exception as e:
+            logger.warning(f"Failed to initialize S3 client: {e}")
+            self.s3_client = None
 
-    s3_client = boto3.client('s3')
+    def upload_file_to_s3(self, file_path: str, object_name: str = None) -> bool:
+        """Uploads a local file to an AWS S3 bucket."""
+        if not self.bucket_name or self.s3_client:
+            logger.info("No bucket name configured or s3 client missing, skipping upload")
+            return False
 
-    try:
-        s3_client.upload_file(file_path, bucket_name, object_name)
-        return True
+        if not os.path.exists(file_path):
+            logger.error(f"File not found: {file_path}")
+            return False
 
-    except FileNotFoundError:
-        logger.error(f"File not found")
-        return False
+        if object_name is None:
+            file_name = os.path.basename(file_path)
+            object_name = f"{self.s3_prefix}{file_name}"
 
-    except NoCredentialsError:
-        logger.error(f"Credentials not available")
-        return False
-    except ClientError as e:
-        logger.error(e)
-        return False
+        try:
+            logger.info(f"Uploading {file_path} to s3://{self.bucket_name}/{object_name}")
+            self.s3_client.upload_file(file_path, self.bucket_name, object_name)
+            logger.info("Uploaded successfully")
+            return True
+
+        except FileNotFoundError:
+            logger.error(f"File not found")
+            return False
+
+        except NoCredentialsError:
+            logger.error(f"Credentials not available")
+            return False
+        except ClientError as e:
+            logger.error(e)
+            return False
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+            return False
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -41,4 +66,6 @@ if __name__ == '__main__':
 
     local_file_path = sys.argv[1]
     bucket_name = sys.argv[2]
-    upload_file_to_s3(local_file_path, bucket_name)
+
+    uploader = Uploader(bucket_name=bucket_name, s3_prefix="")
+    uploader.upload_file_to_s3(local_file_path)
