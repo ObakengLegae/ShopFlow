e9214ce52e71c2936e2015edc35b5e22f2e66664
ObakengLegae <legaeobakeng1@gmail.com>
2026-09-24 21:06:47 +0200

    feat: vpc access
    vpc access given to lambda function

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,6 +22,11 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 data "aws_iam_policy_document" "lambda_s3_read" {
   statement {
     effect = "Allow"
@@ -38,4 +43,9 @@ resource "aws_iam_policy" "lambda_s3_read_policy" {
   name        = "shopflow-lambda-s3-read-policy"
   description = "Allows Lambda to read objects from the ShopFlow data landing zone"
   policy      = data.aws_iam_policy_document.lambda_s3_read.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_read_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_s3_read_policy.arn
 }
